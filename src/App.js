ed7d6f2836f77b3ed986c4204971f3b6cd4c878d
Beshir Saeed <Beshir-saeed@live.com>
2022-10-23 10:26:36 +0300

    Add Login and header components

--- src/App.js
+++ src/App.js
@@ -1,37 +1,53 @@
-import logo from "./logo.svg";
 import Header from "./components/Header";
 import Feed from "./components/Feed";
 import Post from "./components/Post";
 import Login from "./components/Login";
 import Signup from "./components/Signup";
-import { createBrowserRouter, RouterProvider, Route, Link, useParams } from "react-router-dom";
+import {
+	createBrowserRouter,
+	BrowserRouter,
+	RouterProvider,
+	Routes,
+	Route,
+	Link,
+	useParams,
+} from "react-router-dom";
 import "./App.css";
 
 // id={useParams()}
-const router = createBrowserRouter([
-	{
-		path: "/",
-		element: <Feed />,
-	},
-	{
-		path: "/post/:id",
-		element: <Post />,
-	},
-	{
-		path: "/login",
-		element: <Login />,
-	},
-	{
-		path: "/signup",
-		element: <Signup />,
-	},
-]);
+// const router = createBrowserRouter([
+// 	{
+// 		path: "/",
+// 		element: <Feed />,
+// 	},
+// 	{
+// 		path: "/post/:id",
+// 		element: <Post />,
+// 	},
+// 	{
+// 		path: "/login",
+// 		element: <Login />,
+// 	},
+// 	{
+// 		path: "/signup",
+// 		element: <Signup />,
+// 	},
+// ]);
 
 function App() {
 	return (
 		<>
 			<Header />
-			<RouterProvider router={router} />
+			<BrowserRouter>
+				<Routes>
+					<Route path="login" element={<Login />} />
+					<Route path="/" element={<Feed />} />
+					<Route index element={<Login />} />
+					<Route path=":postId" element={<Post />} />
+					<Route path="signup" element={<Signup />} />
+					{/* <Route index element={<LeagueStandings />} /> */}
+				</Routes>
+			</BrowserRouter>
 		</>
 	);
 }
