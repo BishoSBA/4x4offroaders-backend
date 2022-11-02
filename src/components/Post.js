b68f0e1cf361161f3c1e0ae2796696dc84866d2d
Beshir Saeed <Beshir-saeed@live.com>
2022-11-02 11:28:15 +0300

    Feed layout adjustments

--- src/components/Post.js
+++ src/components/Post.js
@@ -15,7 +15,7 @@ const Post = () => {
 	};
 
 	return (
-		<div className="place-self-center p-12">
+		<div className="place-self-center p-6">
 			<figure>
 				<img src={post.image} alt="img" />
 			</figure>
--- src/components/PostCard.js
+++ src/components/PostCard.js
@@ -1,23 +1,27 @@
 import { Link } from "react-router-dom";
 
 const Postcard = ({ post }) => {
-  return (
-    <Link to="/post">
-      <div className="card w-96 bg-base-100 shadow-2xl m-4 hover:scale-105 duration-500">
-        <figure>
-          <img src={post.image} alt="img" />
-        </figure>
-        <div className="card-body">
-          <h2 className="card-title">{post.title}</h2>
-          <div className="card-actions justify-end mt-4">
-            <div className="badge badge-primary">LC79</div>
-            <div className="badge badge-secondary">Fox</div>
-            <div className="badge badge-light">Saudi4x4Center</div>
-          </div>
-        </div>
-      </div>
-    </Link>
-  );
+	return (
+		<Link to="/post">
+			<div className="card w-96 bg-base-100 h-full border border-gray-00 shadow-md p-4 hover:scale-105 duration-500">
+				<div className="flex bg-base-100 h-full w-full">
+					<img
+						src={post.image}
+						alt="img"
+						className="place-self-center w-fit h-fit mx-auto"
+					/>
+				</div>
+				<div className="card-body">
+					<h2 className="card-title">{post.title}</h2>
+					<div className="card-actions justify-end mt-4">
+						<div className="badge badge-primary">LC79</div>
+						<div className="badge badge-secondary">Fox</div>
+						<div className="badge badge-light">Saudi4x4Center</div>
+					</div>
+				</div>
+			</div>
+		</Link>
+	);
 };
 
 export default Postcard;
