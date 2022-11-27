bf02be2e382181187bee721c5f069dede8d3a4d8
Beshir Saeed <Beshir-saeed@live.com>
2022-11-27 11:00:39 +0300

    callback url

--- config/passport.js
+++ config/passport.js
@@ -10,7 +10,7 @@ module.exports = function (passport) {
 			{
 				clientID: process.env.GOOGLE_CLIENT_ID,
 				clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-				callbackURL: "http://localhost:2121/api/auth/google/callback",
+				callbackURL: "/api/auth/google/callback",
 			},
 			async (accessToken, refreshToken, profile, done) => {
 				console.log(profile);
